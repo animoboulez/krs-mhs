4e84bdaf337c5bcce4b84b689e6a6b14ff5ec78f
animoboulez <jibay.2403@gmail.com>
2026-01-15 04:08:25 +0800

    Update blade view pilih

--- resources/views/krs_mahasiswa/pilih.blade.php
+++ resources/views/krs_mahasiswa/pilih.blade.php
@@ -2,65 +2,66 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Mahasiswa</title>
+    <title>Pilih KRS</title>
 </head>
 <body>
-    <h1>Pengambilan KRS (Mahasiswa)</h1>
+    <h1>Pilih KRS (Mahasiswa)</h1>
 
-   <p>
-    NPM: <b>{{ $mahasiswa->npm }}</b><br>
-    Nama: <b>{{ $mahasiswa->nama }}</b>
-</p>
+    @if(session('success'))
+        <p style="color:green">{{ session('success') }}</p>
+    @endif
 
-@if ($errors->any())
-    <div style="color:red;">
-        <ul>
-            @foreach ($errors->all() as $e) <li>{{ $e }}</li> @endforeach
-        </ul>
-    </div>
-@endif
+    <p><b>NPM:</b> {{ $mahasiswa?->npm }}</p>
+    <p><b>Nama:</b> {{ $mahasiswa?->nama }}</p>
 
-<form method="POST" action="{{ route('krs.mahasiswa.simpan') }}">
-    @csrf
-    <input type="hidden" name="npm" value="{{ $mahasiswa->npm }}">
+    <form method="POST" action="{{ route('krs.simpan') }}">
+        @csrf
 
-    <table border="1" cellpadding="8" cellspacing="0">
-        <thead>
-            <tr>
-                <th>Pilih</th>
-                <th>No</th>
-                <th>Hari</th>
-                <th>Jam</th>
-                <th>Ruang</th>
-                <th>Kode MK</th>
-                <th>Dosen</th>
-                <th>Mata Kuliah</th>
-                <th>SKS</th>
-            </tr>
-        </thead>
-        <tbody>
-        @foreach($penawaran as $i => $p)
-            <tr>
-                <td>
-                    <input type="checkbox" name="penawaran_ids[]"
-                        value="{{ $p->id }}"
-                        {{ in_array($p->id, $selectedIds) ? 'checked' : '' }}>
-                </td>
-                <td>{{ $i+1 }}</td>
-                <td>{{ $p->hari }}</td>
-                <td>{{ $p->jam }}</td>
-                <td>{{ $p->ruang }}</td>
-                <td>{{ $p->kode_mk }}</td>
-                <td>{{ $p->dosen }}</td>
-                <td>{{ $p->mata_kuliah }}</td>
-                <td>{{ $p->sks }}</td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
+        <input type="hidden" name="npm" value="{{ $mahasiswa?->npm }}">
 
-    <br>
-    <button type="submit">Simpan KRS</button>
-</form>
+        <label>Tahun Ajaran (opsional):</label>
+        <input type="text" name="tahun_ajaran" placeholder="2025/2026" value="{{ old('tahun_ajaran') }}">
+        <br><br>
+
+        @error('penawaran') <p style="color:red">{{ $message }}</p> @enderror
+
+        <table border="1" cellpadding="8" cellspacing="0">
+            <thead>
+                <tr>
+                    <th>Pilih</th>
+                    <th>No</th>
+                    <th>Hari</th>
+                    <th>Jam</th>
+                    <th>Ruang</th>
+                    <th>Kode MK</th>
+                    <th>Dosen</th>
+                    <th>Mata Kuliah</th>
+                    <th>SKS</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($penawaran as $i => $p)
+                <tr>
+                    <td>
+                        <input type="checkbox" name="penawaran[]" value="{{ $p->id }}">
+                    </td>
+                    <td>{{ $i+1 }}</td>
+                    <td>{{ $p->hari }}</td>
+                    <td>{{ $p->jam }}</td>
+                    <td>{{ $p->ruang }}</td>
+                    <td>{{ $p->kode_mk }}</td>
+                    <td>{{ $p->dosen }}</td>
+                    <td>{{ $p->matakuliah }}</td>
+                    <td>{{ $p->sks }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+        <br>
+        <button type="submit">Simpan KRS</button>
+    </form>
+
+    <p><a href="{{ route('krs.hasil') }}">Lihat KRS Saya</a></p>
 </body>
 </html>
