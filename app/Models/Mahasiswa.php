81d96cfdbcab991bc2afbd4b95f9f9a7bf04bec4
animoboulez <jibay.2403@gmail.com>
2026-01-14 18:23:26 +0800

    Fix Mahasiswa model primary key

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -6,11 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Mahasiswa extends Model
 {
-    protected $table = 'mahasiswa';
+    // protected $table = 'mahasiswa';
 
-    protected $primaryKey = 'npm';   // harus camelCase: primaryKey
+    // protected $primaryKey = 'npm';   // harus camelCase: primaryKey
+    // public $incrementing = false;
+    // protected $keyType = 'string';
+
+    // protected $fillable = ['npm', 'nama', 'prodi', 'angkatan'];
+
+    protected $table = 'mahasiswa';
+    protected $primaryKey = 'npm';
     public $incrementing = false;
     protected $keyType = 'string';
+
+    public $timestamps = false;
 
     protected $fillable = ['npm', 'nama', 'prodi', 'angkatan'];
 }
