fc52c59f89d9cee78cf0b4b57c967f5ddca80824
animoboulez <jibay.2403@gmail.com>
2026-01-15 04:09:35 +0800

    Update blade view hasil

--- resources/views/krs_mahasiswa/hasil.blade.php
+++ resources/views/krs_mahasiswa/hasil.blade.php
@@ -1,55 +1,52 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>KRS</title>
+    <meta charset="utf-8">
+    <title>Hasil KRS</title>
 </head>
 <body>
-    <h1>KRS Mahasiswa</h1>
+    <h1>Hasil KRS (Mahasiswa)</h1>
 
-@if(session('success'))
-    <p style="color:green;">{{ session('success') }}</p>
-@endif
+    @if(session('success'))
+        <p style="color:green">{{ session('success') }}</p>
+    @endif
 
-<p>
-    NPM: <b>{{ $mahasiswa->npm }}</b><br>
-    Nama: <b>{{ $mahasiswa->nama }}</b>
-</p>
+    <p><b>NPM:</b> {{ $mahasiswa?->npm }}</p>
+    <p><b>Nama:</b> {{ $mahasiswa?->nama }}</p>
 
-<a href="{{ route('krs.mahasiswa.pilih', ['npm' => $mahasiswa->npm]) }}">Kembali pilih KRS</a>
+    <table border="1" cellpadding="8" cellspacing="0">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Hari</th>
+                <th>Jam</th>
+                <th>Ruang</th>
+                <th>Kode MK</th>
+                <th>Mata Kuliah</th>
+                <th>SKS</th>
+                <th>Tahun Ajaran</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse($data as $i => $row)
+            <tr>
+                <td>{{ $i+1 }}</td>
+                <td>{{ $row->hari }}</td>
+                <td>{{ $row->jam }}</td>
+                <td>{{ $row->ruang }}</td>
+                <td>{{ $row->kode_mk }}</td>
+                <td>{{ $row->matakuliah }}</td>
+                <td>{{ $row->sks }}</td>
+                <td>{{ $row->tahun_ajaran }}</td>
+            </tr>
+            @empty
+            <tr>
+                <td colspan="8">Belum ada KRS yang dipilih.</td>
+            </tr>
+            @endforelse
+        </tbody>
+    </table>
 
-<br><br>
-
-<table border="1" cellpadding="8" cellspacing="0">
-    <thead>
-        <tr>
-            <th>No</th>
-            <th>Hari</th>
-            <th>Jam</th>
-            <th>Ruang</th>
-            <th>Kode MK</th>
-            <th>Mata Kuliah</th>
-            <th>SKS</th>
-        </tr>
-    </thead>
-    <tbody>
-    @forelse($diambil as $i => $p)
-        <tr>
-            <td>{{ $i+1 }}</td>
-            <td>{{ $p->hari }}</td>
-            <td>{{ $p->jam }}</td>
-            <td>{{ $p->ruang }}</td>
-            <td>{{ $p->kode_mk }}</td>
-            <td>{{ $p->mata_kuliah }}</td>
-            <td>{{ $p->sks }}</td>
-        </tr>
-    @empty
-        <tr><td colspan="7">Belum ada KRS yang diambil.</td></tr>
-    @endforelse
-    </tbody>
-</table>
-
+    <p><a href="{{ route('krs.pilih') }}">Kembali Pilih KRS</a></p>
 </body>
 </html>
